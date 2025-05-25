e9c968a37210249620ad118ab0c5005cfd4788cb
harrybhagat3012 <harshilbhagat30@gmail.com>
2025-05-25 18:10:52 +0530

    Tuples and Dictionary Files

---
+++ Python_Basics/Dictionary.py
@@ -0,0 +1,35 @@
+#group of key & value pairs
+#mutable
+book = {"Book" : "Kafka on Shore", "Author" : "Murakami"}
+print(book)
+print(type(book))
+
+print(book.get("Author"))
+print(book["Book"])
+
+print(book.keys())
+print(book.values())
+
+book["Author"] = "Haruki Murakami"
+print(book)
+
+#for Author in Book:
+ #   print("Key:", Author)
+
+#book.pop()
+print(book)
+#del car["Book"]
+#compare dictionaries with == & !=
+
+
+# Adding a value to the dictionary at run time which we will use in future frame works
+
+dict = {}
+
+dict["firstname"] = "Harshil"
+dict["lastname"] = "Bhagat"
+dict["gender"] = "Male"
+print(dict)
+print(dict["firstname"])
+print(dict["lastname"])
+print(dict["gender"])
---
+++ Python_Basics/Tuples.py
@@ -0,0 +1,26 @@
+#create tuple using ()
+#same as list but immutable
+a = (1,2,3,4,1,2,3,4,5,6,6,4)
+print(a)
+print(type(a))
+print(a[2])
+print(a[-1])
+
+#we cannot use append(), insert(), remove() & pop(), sort(), reverse()
+for i in range(len(a)):
+    print(a[i])
+
+for e in a:
+    print(e)
+
+print (3 in a) # True
+print (10 in a) # False
+print(a.count(4)) # 3
+print(sum(a)) #41
+print(max(a)) #6
+print(min(a)) #1
+print(a.index(3)) #2
+
+b =(1,2,[3,4,5],6,7,8,9)
+c =(1,2,(3,4,5),6,7,8,9)
+print(c[2][1])
